ee94587c78ad4ac3ab532ef94a248c538c4f9318
kiranbangar1216 <kiranbangar1216@gmail.com>
2019-03-18 18:26:15 +0530

    Changes to schema .

--- mongooseModel/Airlines.js
+++ mongooseModel/Airlines.js
@@ -3,8 +3,175 @@ var schema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Regions"
     },
-    contentData: { type: String },
-    availabilityData: { type: String },
-    priceData: { type: String }
+    contentData: {
+        HotelData: {
+            Id: {
+                type: "string"
+            },
+            Name: {
+                type: "string"
+            },
+            Address: {
+                Street: {
+                    type: "string"
+                },
+                City: {
+                    type: "string"
+                },
+                Country: {
+                    type: "string"
+                },
+                Phone: {
+                    type: "string"
+                },
+                Fax: {
+                    type: "string"
+                },
+                Email: {
+                    type: "string"
+                }
+            },
+            GeoInfos: {
+                Location: {
+                    type: "array",
+                    items: {
+                        Name: {
+                            type: "string"
+                        },
+                        Type: {
+                            type: "string"
+                        }
+                    }
+                },
+                GeoCode: {
+                    Latitude: {
+                        type: "string"
+                    },
+                    Longitude: {
+                        type: "string"
+                    }
+                }
+            },
+            Code: {
+                type: "string"
+            },
+            Source: {
+                type: "string"
+            },
+            TourOperatorCode: {
+                type: "string"
+            },
+            TourOperatorName: {
+                type: "string"
+            },
+            Description: {
+                text: {
+                    type: "string"
+                },
+                language: {
+                    type: "string"
+                }
+            }
+        },
+        MultiMedia: {
+            Images: {
+                Image: {
+                    type: "array",
+                    items: {
+                        type: "object"
+                    }
+                }
+            },
+            Videos: {
+                Video: {
+                    type: "array",
+                    items: {}
+                }
+            }
+        },
+        Features: {
+            Feature: {
+                type: "array",
+                items: {}
+            }
+        },
+        RoomData: {
+            Id: {
+                type: "string"
+            },
+            Name: {
+                type: "string"
+            },
+            Description: {
+                type: "object",
+                properties: {
+                    text: {
+                        type: "string"
+                    },
+                    language: {
+                        type: "string"
+                    }
+                }
+            }
+        },
+        // hotel object end
+        MultiMedia: {
+            Images: {
+                Image: {
+                    type: "array",
+                    items: {
+                        type: "object"
+                    }
+                }
+            },
+            Videos: {
+                Video: {
+                    type: "array",
+                    items: {}
+                }
+            }
+        },
+
+        Features: {
+            Feature: [{ items: { type: String } }]
+        }
+    },
+
+    // content Data end
+
+    availabilityData: {
+        hotel: {
+            type: "string"
+        },
+        fromDate: {
+            type: "string"
+        },
+        toDate: {
+            type: "string"
+        },
+        occupancy: {
+            type: "number"
+        },
+        contentProvider: {
+            type: "string"
+        }
+    },
+    priceData: {
+        hotel: {
+            type: "string"
+        },
+        fromDate: {
+            type: "string"
+        },
+        toDate: {
+            type: "string"
+        },
+        occupancy: {
+            type: "number"
+        },
+        contentProvider: {
+            type: "string"
+        }
+    }
 })
 export default mongoose.model("Airlines", schema)
