ba823b7ccd2a5623902534e0120bef55d6630645
kiranbangar1216 <kiranbangar1216@gmail.com>
2019-03-20 17:00:40 +0530

    merge in folder

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,5 @@
 export default {
     port: 90,
-    mongodbUrl:
-        "mongodb://localhost:27017/Travel-IMS?readPreference=primary",
+    mongodbUrl: "mongodb://localhost:27017/Travel-IMS?readPreference=primary",
     url: "https://travelims.wohlig.co.in"
 }
